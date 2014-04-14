64ad94404b6bf597999d4f3d97e667921b7c2f2a
Thunderbones <mistahGoomba@gmail.com>
2014-04-14 08:09:15 -0500

    Added Active Admin and started using session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
   def loadCategories
     @categories = Category.all
   end
+  
+  private
+  def current_user
+    @_current_user ||= session[:current_user_id] &&
+      User.find_by(id: session[:current_user_id])
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,27 +1,29 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.order("slot")
+    @items = Item.order("slot").page(params[:page]).per(20)
   end
   def show
     @item = Item.find(params[:id])
   end
   def categoryIndex
-    @items = Category.find(params[:id]).items
+    @items = Category.find(params[:id]).items.page(params[:page])
     render action: :index
   end
   def newItemsIndex
-    @items = Item.where("created_at > ?", Date.yesterday())
+    @items = Item.where("created_at > ?", Date.yesterday()).page(params[:page])
     render action: :index
   end
   def updatedItemsIndex
-    @items = Item.where("updated_at > ?", Date.yesterday())
+    @items = Item.where("updated_at > ?", Date.yesterday()).page(params[:page])
     render action: :index
   end
   def addToCart
     #add current item to session
+    
     redirect_to action: :viewCart
   end
   def viewCart
+    
     #show all items in session
   end
 end
